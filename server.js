2ecf0a572d948e8e3a70ccc75f1c10404b277910
sagarwtca <sagar.wtca5@gmail.com>
2016-10-22 23:54:28 +0530

    modifies

--- server.js
+++ server.js
@@ -23,6 +23,6 @@ app.use(express.static('public'));
 // console.log("server startedss at",port);
 // });
 
-app.listen(process.env.PORT || 3000, function(){
+app.listen(process.env.PORT, function(){
   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });

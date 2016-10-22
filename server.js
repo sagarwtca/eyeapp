bb3346444cf94e19078a9143ee0f352c92a17031
sagarwtca <sagar.wtca5@gmail.com>
2016-10-22 22:53:07 +0530

    modify

--- server.js
+++ server.js
@@ -11,15 +11,14 @@ app.use(require('express-session')({
   secret : 'keyboard-cat',
   resave : false,
   saveUninitialized : false
-}))
+}));
+app.use(express.static('public'));
 require('./app/config/passport')(passport);
 require('./app/routes/user.server.routes')(app, passport);
 require('./app/routes/product.server.routes')(app);
 
-app.get('/', function(req, res) {
-	res.sendFile(path.join(__dirname + '/public/index.html'))
-})
+
 app.use(express.static('public'));
 app.listen(3000,function(err){
-console.log("server started at",port);
+console.log("server startedss at",port);
 });

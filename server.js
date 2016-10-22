40d3fd091b9b92119d0d674ab358995fdd7bf648
sagarwtca <sagar.wtca5@gmail.com>
2016-10-22 23:02:09 +0530

    edit server.js

--- server.js
+++ server.js
@@ -19,6 +19,10 @@ require('./app/routes/product.server.routes')(app);
 
 
 app.use(express.static('public'));
-app.listen(3000,function(err){
-console.log("server startedss at",port);
+// app.listen(3000,function(err){
+// console.log("server startedss at",port);
+// });
+
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
